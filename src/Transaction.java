a979a2147deac9a2bed2f1f436363b4fadf681f7
shermanturner-robinson <Shermanrob57@gmail.com>
2025-10-17 00:39:04 -0700

    True commit

--- src/Transaction.java
+++ src/Transaction.java
@@ -35,15 +35,7 @@ public class Transaction {
     public double getAmount() {
         return amount;
     }
-    public static Transaction fromCsvString(String Line) {
-        String[] split = Line.split("\\|");
-        LocalDate date = LocalDate.parse(split[0]);
-        LocalTime time = LocalTime.parse(split[1]);
-        String description = split[2];
-        String vendor = split[3];
-        double amount = Double.parseDouble(split[4]);
-        return new Transaction(date, time, description, vendor, amount);
-    }
+
     @Override
     public String toString() {
         return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
